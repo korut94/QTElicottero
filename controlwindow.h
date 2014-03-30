5273b6b0ed244918ac8a3078eeed7e787af3f4a9
korut94 <andreamnt94@gmail.com>
2014-03-30 12:02:30 +0200

    Class ControlWindow
    Finestra con tutti i widget di controllo dell'elicottero

---
+++ controlwindow.h
@@ -0,0 +1,42 @@
+#ifndef CONTROLWINDOW_H
+#define CONTROLWINDOW_H
+
+#include <QMainWindow>
+#include "stick.h"
+#include "button.h"
+
+namespace Ui {
+class ControlWindow;
+}
+
+class ControlWindow : public QMainWindow
+{
+    Q_OBJECT
+
+   public:
+    explicit ControlWindow(QWidget *parent = 0);
+    ~ControlWindow(); 
+
+   protected:
+        void mousePressEvent( QMouseEvent * event );
+        void mouseReleaseEvent( QMouseEvent * event );
+        void mouseMoveEvent( QMouseEvent * event );
+
+   signals:
+        void controlHeli( QPoint & newPos );
+        void stop();
+
+   private:
+    Ui::ControlWindow * ui;
+
+    Stick * joyStick;
+
+    Button * btnLeft;
+    Button * btnRight;
+    Button * btnTop;
+    Button * btnDown;
+
+    bool mouseDrag;
+};
+
+#endif // CONTROLWINDOW_H
